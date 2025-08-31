4857cb80380b6c88d879490e5768fbd8b963e1c8
Pranav Saravanan <prnvsaravanan@gmail.com>
2025-08-30 17:12:39 -0700

    fine tuning model

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 # main.py
-from ultralytics import YOLO
+# from ultralytics import YOLO
 
-model = YOLO("yolov8l.pt")
-   
-results = model.predict("data/raw_videos/video_1.mp4", save=True)
+from utils.video_utils import read_video, save_video
+""" model = YOLO("models/BallDetection.pt")
+results = model.track("data/raw_videos/video_1.mp4", save=True)
 print(results)
 print("====")
 for box in results[0].boxes:
     print(box)
+"""
+def main():
+     
+    #Read video
+     video_frames = read_video("data/raw_videos/video_1.mp4")
+
+     #Save video
+     save_video(video_frames, "output_videos/output_video.avi")
+
+
+if __name__ == '__main__':
+    main()
